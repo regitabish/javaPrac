26ee9300fe37bb62dd0c1f34f41d5b59989ba4fc
regitabish <shiba.taiga.lab@gmail.com>
2024-09-04 16:41:47 +0900

    chapter 6

---
+++ 6/prac6_1/main.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String[] args)throws Exception{
+        zenhanpacage.zenhan.doWarusa();
+        zenhanpacage.zenhan.doTogame();
+        kohanpackage.kohan.callDeae();
+        kohanpackage.kohan.showMondokoro();
+    }
+}
